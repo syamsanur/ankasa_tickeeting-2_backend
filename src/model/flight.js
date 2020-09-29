b4ac2ada97e562a2b2cbf4eba59bd45590b36100
syamsa nur <syamsanur0@gmail.com>
2020-09-30 04:51:00 +0700

    filter_salah

--- src/model/flight.js
+++ src/model/flight.js
@@ -16,25 +16,15 @@ const flight = {
       })
     })
   },
-  getToFromDate: (origin, destination, transit, type_flight, date, class_flight,
-    luggage, meal, wifi, departurea, departureb, arriveda, arrivedb,pricea, priceb,
+  getToFromDate: (origin, destination, type_flight, date, class_flight, luggage, meal, wifi, airlines,
     limit,offset) => {
-      
+      // console.log(origin, destination, type_flight, date, class_flight, luggage, meal, wifi, airlines,
+      //   limit,offset)
       return new Promise ((resolve, reject) => {
-        db.query(`SELECT * FROM v_flight
-        where 
-	        origin = '${origin}' AND
-          destination = '${destination}' AND
-          transit = '${transit}' AND
-          type_flight = '${type_flight}' AND
-          date = '${date}' AND
-          class_flight = '${class_flight}' AND
-          luggage = '${luggage}' AND
-          meal = '${meal}' AND
-          wifi = '${wifi}' AND
-          price BETWEEN '${pricea}' AND '${priceb}' AND
-          departure BETWEEN '${departurea}' AND '${departureb}' AND
-          arrived BETWEEN '${arriveda}' AND '${arrivedb}'
+        db.query(`SELECT * FROM v_flight 
+          WHERE 
+          (origin = "${origin}" AND destination = "${destination}" AND date = "${date}" AND class_flight = ${class_flight}) AND 
+          ((luggage = ${luggage} OR meal = ${meal} OR wifi = ${wifi}) AND (airlines = ${airlines}))
           
           LIMIT ${offset},${limit}`, (err,result)=>{
             if(err){
